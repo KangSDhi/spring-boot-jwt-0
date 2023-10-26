9d805ad225d38e54c812df2489b4cdb40114630c
KangSDhi <kangteknisi@gmail.com>
2023-10-26 12:51:22 +0700

    first commit

---
+++ src/main/java/com/sigit/springbootjwt1/SpringBootJwt1Application.java
@@ -0,0 +1,13 @@
+package com.sigit.springbootjwt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwt1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/sigit/springbootjwt1/user/Role.java
@@ -0,0 +1,6 @@
+package com.sigit.springbootjwt1.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
